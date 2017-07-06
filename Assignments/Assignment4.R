27db232425b6038ee9aebffb117f6b64387e3f5e
Melanie Baybay <baybaymelanie@gmail.com>
2017-07-06 10:46:57 -0400

    adding starter code for assignment 4

---
+++ Assignments/Assignment4.R
@@ -0,0 +1,89 @@
+# -------------------------------------------------------------------------
+# Submitted By: <FULL NAME>
+
+# Assignment4.R - due Wednesday, July 12
+# ICPSR 2017 - Network Analysis I
+
+# Fill in your responses below. For each short answer response, 
+# be sure to include '#' comment notation at the start of each line.
+# -------------------------------------------------------------------------
+
+
+# Question 2 
+# load SNAP data set 
+
+
+# convert to 'igraph' object 
+
+
+##  2a 
+
+
+##  2b  
+
+
+##  2c 
+
+
+##  2d  
+
+
+##  2e  
+
+
+##  2f  
+
+
+
+# Question 3
+# extract 3 subgraphs, 200 random nodes each
+
+
+# - SUBGRAPH 1 - 
+
+
+# - SUBGRAPH 2 - 
+
+
+# - SUBGRAPH 3 - 
+
+
+# network properties : 
+
+
+
+# Question 4
+
+
+
+
+# Question 5
+
+
+
+
+# Question 6
+library(datasets)
+data("iris")
+
+# - ASSOCIATION GRAPH -
+## a
+
+
+## b 
+
+
+## c
+
+
+# - GAUSSIAN GRAPHICAL MODEL -
+## a
+
+
+## b 
+
+
+## c
+
+
+## d
